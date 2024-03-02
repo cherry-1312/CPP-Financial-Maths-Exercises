0bc291bd2e405db740a4cf8b0d5b1dc7de1511c6
cherry-1312 <157616113+cherry-1312@users.noreply.github.com>
2024-03-02 14:23:46 +0000

    Create matlib.h in ch5

---
+++ Chapter 5 Exercises/MyLib/matlib.h
@@ -0,0 +1,15 @@
+//
+//  matlib.h
+//  MyLib
+//
+//  Created by Sebastian Cordoba on 31/01/2024.
+//
+
+#pragma once //prevents same file being included twice
+
+const double PI = 3.14159265358979;
+
+double normcdf(double x);
+
+double norminv(double x);
+
