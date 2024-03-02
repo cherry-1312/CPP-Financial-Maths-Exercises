b98d2b85142a7469a64c6a187ff6f45263fb053f
cherry-1312 <157616113+cherry-1312@users.noreply.github.com>
2024-03-02 14:22:48 +0000

    Create stdafx.h in ch5

---
+++ Chapter 5 Exercises/MyLib/stdafx.h
@@ -0,0 +1,11 @@
+//
+//  stdafx.h
+//  MyLib
+//
+//  Created by Sebastian Cordoba on 31/01/2024.
+//
+
+#pragma once //prevents same file being included twice
+
+#include <cmath>
+#include <iostream>
